63abed263eccb5e461fff77f58bfc5657fa39a45
Joshua Ip <joshuaip@uw.edu>
2020-05-07 02:18:49 -0700

    More shuffling of np arrays and lists

--- cayde/well/nlpwell.py
+++ cayde/well/nlpwell.py
@@ -273,7 +273,7 @@ class NLPWell(Well):
     def createWord2VecFeatures(self, modelLocation, keepUnigrams=False) -> List[str]:
         avail_columns = []
 
-        # model = gensim.models.KeyedVectors.load_word2vec_format(modelLocation, binary=True)
+        model = gensim.models.KeyedVectors.load_word2vec_format(modelLocation, binary=True)
 
         for column in self._text_cols:
 
@@ -294,10 +294,10 @@ class NLPWell(Well):
             # document vector built by adding together all the word vectors
             # using Google's pre-trained word vectors
             def word2Vec(word):
-                # if word in model:
-                #     return model[word]
-                # else:
-                    return [0.] * 300
+                if word in model:
+                    return model[word]
+                else:
+                    return np.array([0.] * 300)
 
             text_vec = unigrams.map(lambda x: list(map(word2Vec, x)))
 
